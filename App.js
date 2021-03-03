5e8fdcee6aadbf7597c967f46488941995d48953
okesseko <59650459+okesseko@users.noreply.github.com>
2021-03-03 13:45:07 +0800

    header

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-
+import {ThemeProvider, Header} from 'react-native-elements';
 import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 const Tab = createMaterialBottomTabNavigator();
+
+const theme = {};
 
 function HomeScreen() {
   return (
@@ -25,46 +27,58 @@ function SettingsScreen() {
 }
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator labeled={false} barStyle={{backgroundColor:'black'}}>
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            tabBarIcon: ({color}) => (
-              <MaterialCommunityIcons name="home" color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Search"
-          component={SettingsScreen}
-          options={{
-            tabBarIcon: ({color}) => (
-              <MaterialIcons name="search" color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Notification"
-          component={HomeScreen}
-          options={{
-            tabBarIcon: ({color}) => (
-              <MaterialCommunityIcons name="bell" color={color} size={26} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={HomeScreen}
-          options={{
-            tabBarIcon: ({color}) => (
-              <MaterialCommunityIcons name="account" color={color} size={26} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <Header
+        backgroundColor="black"
+        leftComponent={{icon: 'menu', color: '#fff'}}
+        centerComponent={{text: 'MY TITLE', style: {color: '#fff'}}}
+        rightComponent={{icon: 'home', color: '#fff'}}
+      />
+      <NavigationContainer>
+        <Tab.Navigator labeled={false} barStyle={{backgroundColor: 'black'}}>
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({color}) => (
+                <MaterialCommunityIcons name="home" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Search"
+            component={SettingsScreen}
+            options={{
+              tabBarIcon: ({color}) => (
+                <MaterialIcons name="search" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Notification"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({color}) => (
+                <MaterialCommunityIcons name="bell" color={color} size={26} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({color}) => (
+                <MaterialCommunityIcons
+                  name="account"
+                  color={color}
+                  size={26}
+                />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 };
 
